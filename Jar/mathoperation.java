c0aa0b9f30d531f173638b277c55decfe7452792
vidyadharBP <vidyadharpirannvar@gmail.com>
2021-07-17 14:54:10 +0530

    executable jar

---
+++ Jar/mathoperation.java
@@ -0,0 +1,18 @@
+
+
+public class mathoperation
+{
+     public void add1(int a,int b)
+	 {
+		 int sum=a+b;
+		 
+		 System.out.println("the sum is "+sum); 
+	 }	
+	 
+	  public void sub1(int a,int b)
+	 {
+		 int sub=a-b;
+		 
+		 System.out.println("the substration is "+sub ); 
+	 }
+}
---
+++ Jar/opretion.java
@@ -0,0 +1,12 @@
+
+
+public class opretion
+{
+	public static void main(String[] args)
+	{
+		mathoperation m1=new mathoperation();
+		
+		m1.add1(18,12);
+		m1.sub1(25,5);
+	}
+}
